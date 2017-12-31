934529c358e1268aea5ea044f04487aecccd2240
JOSHUA T <joshua.toy@ingramcontent.com>
2017-12-30 19:14:04 -0500

    gitignore

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['pk_test_NLYXRqbHr3TLR859XO9BJaDF'],
-  :secret_key      => ENV['sk_test_m5sFVwmv8zA9S4ExgWuzlB8v']
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
