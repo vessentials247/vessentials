8d5717617ffbd714f4fabb25ba852dcf13ef27ee
JOSHUA T <joshua.toy@ingramcontent.com>
2017-12-30 21:59:27 -0500

    add subscriptions:

--- app/models/concerns/stripe_tool.rb
+++ app/models/concerns/stripe_tool.rb
@@ -14,4 +14,12 @@ module StripeTool
       currency: 'usd'
     )
   end
+
+  def self.create_membership(email: email, stripe_token: stripe_token, plan: plan)
+    Stripe::Customer.create(
+      email: email,
+      source: stripe_token,
+      plan: plan
+    )
+  end
 end
