b05d363d77c0b2b5c0093a4afb8c69cdd8ec91e9
JOSHUA T <joshua.toy@ingramcontent.com>
2018-02-25 14:09:02 -0500

    change way assets are imported

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,4 +17,17 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+//= require plugins/jquery-3.2.1.min.js
+//= require plugins/popper.min.js
+//= require plugins/bootstrap/js/bootstrap.min.js
+//= require plugins/back-to-top.js
+//= require plugins/jquery-inview/jquery.inview.min.js
+//= require plugins/isMobile/isMobile.min.js
+//= require plugins/flexslider/jquery.flexslider-min.js
+//= require js/main.js
+//= require js/modal-video.js
+//= require js/animations.js
+
+
 //= require_tree .
