f04a561f78ea2962ac06b52b99bb3ef115d5f062
Joshua S. Toy <joshua.toy@ingramcontent.com>
2018-10-07 13:50:36 -0400

    start subscription code

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,25 +4,42 @@ class ChargesController < ApplicationController
   before_action :set_plan
 
   def new
+    @plans = { "value1" => 1, "value2" => 2 }
   end
 
   def thanks
   end
 
   def create
+    # @plan = params[:selected_plan]
     # Amount in cents
-    @mens_small = 2499
+    @mens_small = 2999
+    @womens_small = 2999
 
     customer = Stripe::Customer.create(
       :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
+      :source  => params[:stripeToken],
     )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @mens_small,
-      :description => 'Mens Small',
-      :currency    => 'usd'
+    # subscription = Stripe::Plan.create(
+    #   usage_type: 'licensed',
+    #   currency: 'usd',
+    #   interval: 'month',
+    #   product: 'prod_Djx54WxAITEwES',
+    #   nickname: 'Pro Plan',
+    #   amount: 3000,
+    # )
+
+    # charge = Stripe::Charge.create(
+    #   :customer    => customer.id,
+    #   :amount      => @mens_small,
+    #   :description => 'Mens Small one off',
+    #   :currency    => 'usd'
+    # )
+s
+    subscription = Stripe::Subscription.create(
+      :customer => customer.id,
+      :plan => "plan_Djx6Gwr5kXOFPq"
     )
 
   rescue Stripe::CardError => e
